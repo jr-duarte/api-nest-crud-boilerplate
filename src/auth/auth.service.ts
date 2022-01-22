a0ea1f04ebd938cc4895687a85c9c9e77ec4c923
Junior Duarte <jr.duartee.93@gmail.com>
2022-01-21 21:58:45 -0300

    chore: add no-unused-vars off no eslint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
